e9c15761144784e94eeda0f68e9cf5a44209b68c
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-06-06 15:54:05 +0300

    created splash screen in main Activity

--- app/src/main/java/com/example/bluchat/MainActivity.java
+++ app/src/main/java/com/example/bluchat/MainActivity.java
@@ -1,13 +1,92 @@
 package com.example.bluchat;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+ Button buttonOn, buttonOff;
+ BluetoothAdapter myBluetoothAdapter;
+
+ Intent btEnablingIntent;
+ int requestCodeForEnable;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+
+        },SPLASH_TIME_OUT );
+
+        buttonOn =  (Button) findViewById(R.id.btOn);
+        buttonOff = (Button) findViewById(R.id.btOff);
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        btEnablingIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        requestCodeForEnable = 1;
+
+
+
+        bluetoothOnMethod();
+        bluetoothOffMethod();
+    }
+
+    private void bluetoothOffMethod() {
+        buttonOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(myBluetoothAdapter.isEnabled())
+                {
+                     myBluetoothAdapter.disable();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==requestCodeForEnable)
+        {
+            if(resultCode==RESULT_OK)
+            {
+                Toast.makeText(getApplicationContext(),"Bluetooth is Enabled",Toast.LENGTH_LONG ).show();
+            } else if(resultCode == RESULT_CANCELED)
+            {
+                Toast.makeText(getApplicationContext(),"Bluetooth Enabling Cancelled",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void bluetoothOnMethod() {
+        buttonOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(myBluetoothAdapter==null)
+                {
+                    Toast.makeText(getApplicationContext(),"Bluetooth not supported on this Device",Toast.LENGTH_LONG).show();
+                } else
+                {
+                    if(!myBluetoothAdapter.isEnabled())
+                    {
+                        startActivityForResult(btEnablingIntent,requestCodeForEnable);
+                    }
+                }
+            }
+        });
     }
 }
